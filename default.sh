5ae094abf603884a2c9e031e64a1df0880565597
Sergey Novikov <svnovikov@gmail.com>
2018-03-05 17:56:23 +0100

    Improve bash wrapper

--- default.sh
+++ default.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-ansible-playbook -i "localhost," -c local --ask-become-pass default.yml
+ansible-playbook \
+  --inventory="localhost," \
+  --connection=local \
+  --ask-become-pass \
+  $([ -n $1 ] && echo "--tags=$1")\
+  $([ "$2" == "list" ] && echo "--list-tasks")\
+  default.yml
